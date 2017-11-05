df4ac9ad5dcef18e90c4809404e88ed7df6657dc
soundingWell <maxtheengineer@gmail.com>
2017-11-05 12:56:03 -0800

    Cleanup home page.

--- main.py
+++ main.py
@@ -1,104 +1,45 @@
 #!/usr/bin/python
 
-### Basics ###
-import os
+# Builtin
+import webapp2
 import sys
-import time
 sys.path.append('python/')
 
 # My Stuff
-##########
-#from sport import sport_parser
 from updateAccounts import updateAccounts
 from nhl import nhlPage
 from nba import nbaPage
 from newSports import nflPage
 from newSports import ncbPage
 from newSports import mlbPage
-##########
+from flyer_profile import profilePage
+from flyer_profile import showAllProfilesPage
+from gAccounts import *
+from mUser import mUserStats
 
-# Google Stuff 
-##########
-import appengine_config
-from google.appengine.ext import ndb
-from google.appengine.ext import webapp
-from google.appengine.ext.webapp.util import run_wsgi_app
+# Google
 from google.appengine.ext.webapp import template
 from google.appengine.api import users
-##########
 
-# Session Stuff
-##########
+# Session
 from gaesessions import get_current_session
-##########
-
-# Web Stuff
-########## 
-import webapp2
-import json 
-##########
-
-from gAccounts import *
-from __builtin__ import False
-
-# [END imports]
-
-providers = {
-    'Google'   : 'https://www.google.com/accounts/o8/id'
-}
-
-DEFAULT_NICKNAME= "no_name"
-
-def mUserStats_key(userEmail=DEFAULT_NICKNAME):
-    """Constructs a Datastore key for a user entity with user email."""
-    return ndb.Key('mUserStats', userEmail)
-    
-def render_template(h, aFile, template_vals):
-    path = os.path.join(os.path.dirname(__file__), 'html', aFile)
-    h.response.out.write(template.render(path, template_vals))
-
-def redirect_with_msg(h, msg, dst='/'):
-    get_current_session()['msg'] = msg
-    h.redirect(dst)
-    
-
-class AboutHandler(webapp2.RequestHandler):
-    def get(self):
-        session = get_current_session()
-        current_user = session['me']
-        print 'curr: '
-        print current_user.m_balance
-        
-        template_values = {}
-        path = 'html/login.html'
-        self.response.out.write(template.render(path, template_values))
 
 
 class MainPage(webapp2.RequestHandler):
     def handle_account(self, gae_status, flyer_status):
-        session = get_current_session()
-        
-        # No Google account
+        # No Google account, force Google login.
         if not gae_status:
-            print 'Forcing google login'
             self.redirect(users.create_login_url(self.request.uri))
 
         # Have Google Account, but first time at Flyer
         elif gae_status and not flyer_status:
-            print 'asking them to make an account.'
             print str(gae_status)
             self.redirect('/createAccount')
 
-        # Good to go. 
+        # They are logged into Google and have a Flyer account.
         else:
-            gae_user = users.get_current_user()
-            print gae_user.email()
-            curr_user = is_email_in_our_db(gae_user.email())
-            if curr_user:
-                print 'session started'
-                session['me'] = curr_user
-            if not curr_user:
-                print 'couldnt find user' 
+            print 'Logged in'
+        
                 
         
     def get(self):
@@ -114,81 +55,7 @@ class MainPage(webapp2.RequestHandler):
     def post(self):
         # The user hit logout. They need to login. 
         self.redirect(users.create_logout_url("/"))
-        
-class ShowAllProfiles(webapp2.RequestHandler):
-    def get(self):
-        # Wait for database to update if a bet was just entered.
-        time.sleep(0.2)
-        session = get_current_session()
-        gprofiles = mUserStats.query()
-        profile_counter = 0 
-        for profile in gprofiles:
-            self.response.write('<b>' 'count: </b>' + str(profile_counter) + '<br />')
-            self.response.write('<b>' 'name: </b>' + str(profile.m_team_name) + '<br />')
-            self.response.write('<b> Email: </b>' + str(profile.m_email) + '<br />')
-            if profile.m_currBets:
-                print 'bets: ' + str(len(profile.m_currBets))
-                for event in profile.m_currBets:
-                    for bet in profile.m_currBets[event]:
-                        self.response.write(bet.pick + 
-                            '<b> spread: </b> ' + str(bet.spread) + 
-                            '<b> odds: </b> ' + str(bet.odds) + 
-                            '<b> amount: $</b>' + str(bet.bet_amount) +  '<br />') 
-            profile_counter += 1
 
-
-class Profile(webapp2.RequestHandler):
-    # This lists the current user's profile.
-    def get(self):
-        # Wait for database to update if a bet was just entered.
-        time.sleep(0.2)
-        session = get_current_session()
-        gprofiles = mUserStats.query()
-        for one_user in gprofiles:
-            if one_user.m_email == session['me'].m_email:
-                session['me'] = one_user
-        
-        if session.has_key('me'):
-            curr_user = session['me']
-            self.response.write('<b> Email: </b>' + str(curr_user.m_email) + '<br />')
-            self.response.write('<b> Username: </b>' + str(curr_user.m_nickName) + '<br />')
-            self.response.write('<b> Bets made: </b>' + str(curr_user.m_betsMade) + '<br />')
-            self.response.write('<b> Bets won: </b>' + str(curr_user.m_betsWon) + '<br />')
-            self.response.write('<b> Balance: </b>' + '$' + str(curr_user.m_balance) + '<br />')
-            self.response.write('<b> bets: </b>' + '<br />')
-
-            for event in curr_user.m_currBets:
-                for bet in curr_user.m_currBets[event]:
-                    self.response.write(bet.pick + 
-                                        '<b> spread: </b> ' + str(bet.spread) + 
-                                        '<b> odds: </b> ' + str(bet.odds) + 
-                                        '<b> amount: $</b>' + str(bet.bet_amount) +  '<br />') 
-            
-            self.response.write('<b> Past Bets: </b>' + '<br />')
-            
-            for old_event in curr_user.m_pastBets:
-                for bet in curr_user.m_pastBets[old_event]:
-                    self.response.write(bet.pick + 
-                                        '<b> spread: </b> ' + str(bet.spread) + 
-                                        '<b> odds: </b> ' + str(bet.odds) + 
-                                        '<b> amount: $</b>' + str(bet.bet_amount) +
-                                        '<b> Outcome: </b>' + str(bet.outcome) +  '<br />')  
-            
-        else:
-            redirect_with_msg(self, 'Not logged in', '/login')
-
-    
-    def post(self):
-        # Right now this will delete this user's account
-        session = get_current_session()
-        if (session.has_key('me')):
-            curr_user = session['me']
-            userDB_key = mUserStats_key(curr_user.m_email)
-            mUserStats.get_by_id(curr_user.m_email, userDB_key).delete()
-            session.terminate()
-        else:
-            self.response.write('ERROR')
-            
 
 # This is supposed to to start a thread that will check on the status 
 # of games at a regular interval and update accounts as games finish.
@@ -201,14 +68,13 @@ class startChecking(webapp2.RequestHandler):
 
 app = webapp2.WSGIApplication([
                                        ('/', MainPage),
-                                       ('/about', AboutHandler),
                                        ('/logout', LogoutHandler),
                                        ('/authenticated', AuthenticationHandler),
                                        ('/resetpassword', LogoutAuth),
                                        ('/deleteProfiles', deleteProfiles),
                                        ('/createAccount', CreateAccount),
-                                       ('/profile', Profile),
-                                       ('/showAllProfiles', ShowAllProfiles),
+                                       ('/profile', profilePage),
+                                       ('/showAllProfiles', showAllProfilesPage),
                                        ('/nfl', nflPage),
                                        ('/nhl', nhlPage),
                                        ('/nba', nbaPage),
